90abe9ce034752b98ed89ab584e85de51e6ae461
yyang23 <yyang24@ivytech.edu>
2023-03-16 18:17:49 -0400

    Add project files.

---
+++ FinalPart2/Form1.Designer.cs
@@ -0,0 +1,381 @@
+﻿
+namespace FinalPart2
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.instructionLabel = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.line1CTextBox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.line1BTextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.line1ATextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.line2CTextBox = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.line2BTextBox = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.line2ATextBox = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.checkIntersectionBtn = new System.Windows.Forms.Button();
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.Save = new System.Windows.Forms.Button();
+            this.Load = new System.Windows.Forms.Button();
+            this.clearBtn = new System.Windows.Forms.Button();
+            this.outputTextBox = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // instructionLabel
+            // 
+            this.instructionLabel.AutoSize = true;
+            this.instructionLabel.BackColor = System.Drawing.SystemColors.Window;
+            this.instructionLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.instructionLabel.Location = new System.Drawing.Point(4, 9);
+            this.instructionLabel.Name = "instructionLabel";
+            this.instructionLabel.Size = new System.Drawing.Size(207, 25);
+            this.instructionLabel.TabIndex = 0;
+            this.instructionLabel.Text = "A line is Ax + By = C";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.Lavender;
+            this.groupBox1.Controls.Add(this.label9);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.line1CTextBox);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.line1BTextBox);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.line1ATextBox);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(44, 63);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(350, 122);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Set line1";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(188, 83);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(40, 24);
+            this.label9.TabIndex = 7;
+            this.label9.Text = "y = ";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(107, 83);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(41, 24);
+            this.label8.TabIndex = 6;
+            this.label8.Text = "x + ";
+            // 
+            // line1CTextBox
+            // 
+            this.line1CTextBox.Location = new System.Drawing.Point(234, 83);
+            this.line1CTextBox.Name = "line1CTextBox";
+            this.line1CTextBox.Size = new System.Drawing.Size(30, 29);
+            this.line1CTextBox.TabIndex = 5;
+            this.line1CTextBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TextBox_KeyPress);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(213, 42);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(73, 24);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "C value";
+            // 
+            // line1BTextBox
+            // 
+            this.line1BTextBox.Location = new System.Drawing.Point(154, 83);
+            this.line1BTextBox.Name = "line1BTextBox";
+            this.line1BTextBox.Size = new System.Drawing.Size(28, 29);
+            this.line1BTextBox.TabIndex = 3;
+            this.line1BTextBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TextBox_KeyPress);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(130, 42);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 24);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "B value";
+            // 
+            // line1ATextBox
+            // 
+            this.line1ATextBox.Location = new System.Drawing.Point(73, 83);
+            this.line1ATextBox.Name = "line1ATextBox";
+            this.line1ATextBox.Size = new System.Drawing.Size(28, 29);
+            this.line1ATextBox.TabIndex = 1;
+            this.line1ATextBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TextBox_KeyPress);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(46, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 24);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "A value";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.Color.Lavender;
+            this.groupBox2.Controls.Add(this.label11);
+            this.groupBox2.Controls.Add(this.label10);
+            this.groupBox2.Controls.Add(this.line2CTextBox);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.line2BTextBox);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.line2ATextBox);
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.Location = new System.Drawing.Point(44, 208);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(350, 140);
+            this.groupBox2.TabIndex = 7;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Set line2";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(188, 82);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(35, 24);
+            this.label11.TabIndex = 7;
+            this.label11.Text = "y =";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(107, 82);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(41, 24);
+            this.label10.TabIndex = 6;
+            this.label10.Text = "x + ";
+            // 
+            // line2CTextBox
+            // 
+            this.line2CTextBox.Location = new System.Drawing.Point(235, 82);
+            this.line2CTextBox.Name = "line2CTextBox";
+            this.line2CTextBox.Size = new System.Drawing.Size(29, 29);
+            this.line2CTextBox.TabIndex = 5;
+            this.line2CTextBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TextBox_KeyPress);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(213, 50);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(73, 24);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "C value";
+            // 
+            // line2BTextBox
+            // 
+            this.line2BTextBox.Location = new System.Drawing.Point(154, 82);
+            this.line2BTextBox.Name = "line2BTextBox";
+            this.line2BTextBox.Size = new System.Drawing.Size(29, 29);
+            this.line2BTextBox.TabIndex = 3;
+            this.line2BTextBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TextBox_KeyPress);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(130, 50);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(72, 24);
+            this.label6.TabIndex = 2;
+            this.label6.Text = "B value";
+            // 
+            // line2ATextBox
+            // 
+            this.line2ATextBox.Location = new System.Drawing.Point(73, 82);
+            this.line2ATextBox.Name = "line2ATextBox";
+            this.line2ATextBox.Size = new System.Drawing.Size(29, 29);
+            this.line2ATextBox.TabIndex = 1;
+            this.line2ATextBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TextBox_KeyPress);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(46, 50);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(73, 24);
+            this.label7.TabIndex = 0;
+            this.label7.Text = "A value";
+            // 
+            // checkIntersectionBtn
+            // 
+            this.checkIntersectionBtn.BackColor = System.Drawing.Color.Plum;
+            this.checkIntersectionBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkIntersectionBtn.ForeColor = System.Drawing.SystemColors.Info;
+            this.checkIntersectionBtn.Location = new System.Drawing.Point(109, 367);
+            this.checkIntersectionBtn.Name = "checkIntersectionBtn";
+            this.checkIntersectionBtn.Size = new System.Drawing.Size(221, 41);
+            this.checkIntersectionBtn.TabIndex = 8;
+            this.checkIntersectionBtn.Text = "Check Line Relationship";
+            this.checkIntersectionBtn.UseVisualStyleBackColor = false;
+            this.checkIntersectionBtn.Click += new System.EventHandler(this.CheckLineRelationship_Click);
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.AutoSize = true;
+            this.outputLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputLabel.ForeColor = System.Drawing.Color.RoyalBlue;
+            this.outputLabel.Location = new System.Drawing.Point(44, 408);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(57, 24);
+            this.outputLabel.TabIndex = 9;
+            this.outputLabel.Text = "Output";
+            this.outputLabel.UseCompatibleTextRendering = true;
+            // 
+            // Save
+            // 
+            this.Save.BackColor = System.Drawing.Color.SteelBlue;
+            this.Save.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Save.ForeColor = System.Drawing.SystemColors.Info;
+            this.Save.Location = new System.Drawing.Point(424, 142);
+            this.Save.Name = "Save";
+            this.Save.Size = new System.Drawing.Size(75, 41);
+            this.Save.TabIndex = 10;
+            this.Save.Text = "Save";
+            this.Save.UseVisualStyleBackColor = false;
+            this.Save.Click += new System.EventHandler(this.Save_Click);
+            // 
+            // Load
+            // 
+            this.Load.BackColor = System.Drawing.Color.SteelBlue;
+            this.Load.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Load.ForeColor = System.Drawing.SystemColors.Info;
+            this.Load.Location = new System.Drawing.Point(424, 208);
+            this.Load.Name = "Load";
+            this.Load.Size = new System.Drawing.Size(75, 41);
+            this.Load.TabIndex = 11;
+            this.Load.Text = "Load";
+            this.Load.UseVisualStyleBackColor = false;
+            this.Load.Click += new System.EventHandler(this.Load_Click);
+            // 
+            // clearBtn
+            // 
+            this.clearBtn.BackColor = System.Drawing.Color.SteelBlue;
+            this.clearBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.clearBtn.ForeColor = System.Drawing.Color.OrangeRed;
+            this.clearBtn.Location = new System.Drawing.Point(424, 273);
+            this.clearBtn.Name = "clearBtn";
+            this.clearBtn.Size = new System.Drawing.Size(75, 41);
+            this.clearBtn.TabIndex = 12;
+            this.clearBtn.Text = "Clear";
+            this.clearBtn.UseVisualStyleBackColor = false;
+            this.clearBtn.Click += new System.EventHandler(this.Clear_Click);
+            // 
+            // outputTextBox
+            // 
+            this.outputTextBox.BackColor = System.Drawing.Color.Thistle;
+            this.outputTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputTextBox.Location = new System.Drawing.Point(44, 435);
+            this.outputTextBox.Multiline = true;
+            this.outputTextBox.Name = "outputTextBox";
+            this.outputTextBox.ReadOnly = true;
+            this.outputTextBox.Size = new System.Drawing.Size(350, 67);
+            this.outputTextBox.TabIndex = 13;
+            this.outputTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(530, 514);
+            this.Controls.Add(this.outputTextBox);
+            this.Controls.Add(this.clearBtn);
+            this.Controls.Add(this.Load);
+            this.Controls.Add(this.Save);
+            this.Controls.Add(this.outputLabel);
+            this.Controls.Add(this.checkIntersectionBtn);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.instructionLabel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.Text = "MainForm";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label instructionLabel;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox line1CTextBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox line1BTextBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox line1ATextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox line2CTextBox;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox line2BTextBox;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox line2ATextBox;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button checkIntersectionBtn;
+        private System.Windows.Forms.Label outputLabel;
+        private System.Windows.Forms.Button Save;
+        private System.Windows.Forms.Button Load;
+        private System.Windows.Forms.Button clearBtn;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox outputTextBox;
+    }
+}
+
---
+++ FinalPart2/Form1.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FinalPart2
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }       
+
+        private void CheckLineRelationship_Click(object sender, EventArgs e)
+        {
+            Line[] lines = CreateTwoLinesArray();
+            if(lines != null)
+                DecideLineRelationship(lines);
+            return;            
+        }
+        private Line[] CreateTwoLinesArray() 
+        {
+            Line[] lines = new Line[2]; // Create an array including two lines.
+            try
+            {
+                lines[0] = new Line(Convert.ToDouble(line1ATextBox.Text), Convert.ToDouble(line1BTextBox.Text),
+                Convert.ToDouble(line1CTextBox.Text));
+                lines[1] = new Line(Convert.ToDouble(line2ATextBox.Text), Convert.ToDouble(line2BTextBox.Text),
+                    Convert.ToDouble(line2CTextBox.Text));
+            }
+            catch(Exception ex)
+            {
+                DialogResult answer;
+                string exceptionMessage = ex.Message;
+                if (ex is LineException)
+                    exceptionMessage = ((LineException)ex).Message;
+                answer = MessageBox.Show(exceptionMessage, "Input Error",
+                    MessageBoxButtons.RetryCancel, MessageBoxIcon.Information);
+                if (answer == DialogResult.Retry)
+                {
+                    if (((line1ATextBox.Text == "" || Convert.ToDouble(line1ATextBox.Text) == 0)
+                        && (line1BTextBox.Text == "" || Convert.ToDouble(line1BTextBox.Text) == 0))
+                        || line1CTextBox.Text == "")
+                    {
+                        line1ATextBox.Text = "";
+                        line1BTextBox.Text = "";
+                        line1CTextBox.Text = "";
+                    }
+                    if (((line2ATextBox.Text == "" || Convert.ToDouble(line2ATextBox.Text) == 0)
+                        && (line2BTextBox.Text == "" || Convert.ToDouble(line2BTextBox.Text) == 0))
+                        || line2CTextBox.Text == "")
+                    {
+                        line2ATextBox.Text = "";
+                        line2BTextBox.Text = "";
+                        line2CTextBox.Text = "";
+                    }
+                }
+                else
+                    Close();
+                return null;
+            }            
+            return lines;            
+        }
+        private void FillTwoLinesBoxes(Line[] lines)
+        {
+            line1ATextBox.Text = lines[0].A.ToString();
+            line1BTextBox.Text = lines[0].B.ToString();
+            line1CTextBox.Text = lines[0].C.ToString();
+            line2ATextBox.Text = lines[1].A.ToString();
+            line2BTextBox.Text = lines[1].B.ToString();
+            line2CTextBox.Text = lines[1].C.ToString();
+        }
+        private void DecideLineRelationship(Line[] lines)
+        {
+            //Check parallell or not
+            if (lines[0].A * lines[1].B == lines[0].B * lines[1].A)  
+            {
+                if (lines[0].B * lines[1].C == lines[0].C * lines[1].B)
+                {
+                    outputTextBox.Text = "Same line.";
+                }
+                else
+                {
+                    outputTextBox.Text = "The two lines are parallel.";
+                }
+            }
+            else
+            {
+                double intersactionX = (lines[0].B * lines[1].C - lines[1].B * lines[0].C) / (lines[0].B * lines[1].A - lines[1].B * lines[0].A);
+                double intersactionY = (lines[0].A * lines[1].C - lines[1].A * lines[0].C) / (lines[0].A * lines[1].B - lines[1].A * lines[0].B);
+                outputTextBox.Text = $"The two lines has a intersection. \nThe solution is ({intersactionX}, {intersactionY})";
+            }
+            return;
+        }
+        private void Save_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Title = "Please select a folder to save";
+            sfd.InitialDirectory = Application.StartupPath;
+            sfd.Filter = "ser|*.ser";
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                string FILENAME = sfd.FileName;
+                FileStream outFile = new FileStream(FILENAME, FileMode.Create, FileAccess.Write);
+                BinaryFormatter bFormatter = new BinaryFormatter();
+                bFormatter.Serialize(outFile, CreateTwoLinesArray());
+                outFile.Close();
+            }
+        }
+
+        private void Load_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Title = "Please select a file to load";
+            ofd.InitialDirectory = Application.StartupPath;
+            ofd.Filter = "txt|*.txt|ser|*.ser|all|*.*";
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                string FILENAME = ofd.FileName;
+                FileStream inFile = new FileStream(FILENAME, FileMode.Open, FileAccess.Read);
+                BinaryFormatter bFormatter = new BinaryFormatter();
+                while (inFile.Position < inFile.Length)
+                {
+                    Line[] lines = (Line[])bFormatter.Deserialize(inFile);
+                    FillTwoLinesBoxes(lines);
+                }
+                inFile.Close();
+            }
+        }
+
+        private void Clear_Click(object sender, EventArgs e)
+        {
+            DialogResult answer;
+            
+            answer = MessageBox.Show("Do you want to clear ALL the values in the boxes?", "Warning", 
+                MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+            if (answer == DialogResult.Yes)
+            {
+                line1ATextBox.Text = null;
+                line1BTextBox.Text = null;
+                line1CTextBox.Text = null;
+                line2ATextBox.Text = null;
+                line2BTextBox.Text = null;
+                line2CTextBox.Text = null;
+                outputTextBox.Text = null;
+            }
+        }
+        private void TextBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // Allow digit and '.' to input.
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) &&
+                (e.KeyChar != '.'))
+            {
+                e.Handled = true;
+            }
+
+            // Only allow one decimal point.
+            if ((e.KeyChar == '.') && ((sender as TextBox).Text.IndexOf('.') > -1))
+            {
+                e.Handled = true;
+            }
+        }
+    }
+
+    class LineException: Exception
+    {
+        string message;
+        public string Message
+        {
+            get
+            {
+                return message;
+            }
+            set
+            {
+                message = value;
+            }
+        }
+    }
+
+    [Serializable]
+    class Line
+    {
+        // line is Ax + By = C
+        // A is variable x's coefficient
+        // B is variable y's coefficient
+        // C is constant coefficient
+        public double A { get; set; }
+        public double B { get; set; }
+        public double C { get; set; } 
+        public Line(double coefficientOfX, double coefficientOfY, double constCoefficient)
+        {
+            this.A = coefficientOfX;
+            this.B = coefficientOfY;
+            this.C = constCoefficient;
+            if(this.A == 0 && this.B == 0)
+            {
+                LineException notLine = new LineException();
+                notLine.Message = "For a line, values A and B cannot be zero at the same time.";
+                throw (notLine);
+            }
+        }
+    }
+
+}
